7930a642a2cd25a02c23b104392b36ca4c384112
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-03-31 22:23:26 -0500

    Added more sinks for taint analysis and more benchmarks

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export CLASSPATH=soot/axml-2.0.jar:soot/slf4j-api-1.7.5.jar:soot/slf4j-simple-1.7.5.jar:soot/soot-infoflow-android.jar:soot/soot-infoflow.jar:soot/soot-trunk.jar:.
 
-java ReDoSDetector benchmarks/Venmo_v6.24.1_apkpure.com.apk
+java ReDoSDetector benchmarks/Venmo_v6.24.2_apkpure.com.apk
