730145dea98fa779a3e8111917dad2855b799e99
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-04-02 22:06:31 -0500

    Changed run script to take parameter APK

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 export CLASSPATH=soot/axml-2.0.jar:soot/slf4j-api-1.7.5.jar:soot/slf4j-simple-1.7.5.jar:soot/soot-infoflow-android.jar:soot/soot-infoflow.jar:soot/soot-trunk.jar:.
 
-java ReDoSDetector benchmarks/Venmo_v6.24.2_apkpure.com.apk
+APK_FILE=$1
+
+if [ -z "$APK_FILE" ]; then
+    echo "Usage: ./run.sh <apk_file>"
+    exit 1
+fi
+
+java ReDoSDetector ${APK_FILE}
