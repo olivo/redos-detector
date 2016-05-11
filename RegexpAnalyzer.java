799ec4cf2ae1f08d3bb158f2f5cbb22229fc7c3b
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-05-10 23:36:29 -0500

    Added basic method for checking whether a regular expression is bad

--- RegexpAnalyzer.java
+++ RegexpAnalyzer.java
@@ -1,4 +1,14 @@
+import java.util.HashMap;
 class RegexAnalyzer {
 
+    HashMap<String, Boolean> cache = new HashMap<String, Boolean>();
+
+    public boolean isEvilRegex(String regex){
+	if(cache.containsKey(regex)){
+	    return cache.get(regex);
+	}
+	cache.put(regex, false);
+	return false;
+    }
 
 }
