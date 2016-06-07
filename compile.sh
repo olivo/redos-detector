70c500c9caffd3b29b6faa69168dabae0ddb331e
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-06-07 18:31:05 -0500

    Added compilation of RXXR2 regex checker to main compilation script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 export CLASSPATH=soot/axml-2.0.jar:soot/slf4j-api-1.7.5.jar:soot/slf4j-simple-1.7.5.jar:soot/soot-infoflow-android.jar:soot/soot-infoflow.jar:soot/soot-trunk.jar:.
 
 javac *.java
+cd regex_checker/rxxr/code
+./build.sh
