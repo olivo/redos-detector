515227207f84ca2a6a9ee070e6742ded0a76a353
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-06-16 17:29:53 -0500

    Reading output of RXXR in RegexAnalyzer class

--- RegexAnalyzer.java
+++ RegexAnalyzer.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
@@ -54,13 +55,20 @@ class RegexAnalyzer {
 	    // Read the output of RXXR.
 	    File outFile = new File("regex_out.txt");
 	    FileReader fr = new FileReader(outFile);
+	    BufferedReader br = new BufferedReader(fr);
+	    String line;
+	    boolean vulnerable = false;
+	    while((line = br.readLine()) != null){
+		if(line.contains("VULNERABLE: YES")){
+		    vulnerable = true;
+		    break;
+		}
+	    }
+	    cache.put(regex, vulnerable);
+	    return vulnerable;
 	} catch(IOException e){
 	    System.out.println("Could not run the RXXR regex checker.");
 	    return false;
 	}
-
-	cache.put(regex, false);
-	return false;
     }
-
 }
