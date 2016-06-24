470711a1103a2cb3d0805f8e7094bf83a04b8e08
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-06-23 23:50:02 -0500

    Added analyses of pattern compilations to identify sources of taint from evil regexes

--- TaintWrapper.java
+++ TaintWrapper.java
@@ -10,8 +10,11 @@ import soot.jimple.infoflow.data.AccessPath;
 import soot.jimple.infoflow.data.AccessPathFactory;
 import soot.jimple.infoflow.taintWrappers.AbstractTaintWrapper;
 import soot.jimple.infoflow.taintWrappers.EasyTaintWrapper;
+import soot.jimple.internal.JimpleLocalBox;
 import soot.SootMethod;
+import soot.UnitBox;
 import soot.Value;
+import soot.ValueBox;
 
 class TaintWrapper extends AbstractTaintWrapper {
 
@@ -52,10 +55,27 @@ class TaintWrapper extends AbstractTaintWrapper {
 	// Propagating taint from easy taint wrapper.
 	taints.addAll(easyTaintWrapper.getTaintsForMethodInternal(stmt, taintedPath));
 
+	// Adding source of taint from compilation of malicious pattern.
+	if(method.getSignature().equals("<java.util.regex.Pattern: java.util.regex.Pattern compile(java.lang.String)>")){
+
+	    System.out.println("POTENTIAL SOURCE: " + stmt);
+
+	    // Get the regular expression string from the pattern compilation.
+	    InvokeExpr invokeExpr = stmt.getInvokeExpr();
+	    String regex = getRegex(invokeExpr);
+	    // Check if the regular expression is evil.
+	    if(this.regexAnalyzer.isEvilRegex(regex)){
+		    System.out.println("EVIL REGEX: Potential REDoS source found: " + stmt);
+	    }
+	}
+
 	// Propagating custom taint over the receiving object
 	// of a call to put with a tainted value.
 	if(method.getSignature().equals("<java.util.regex.Pattern: java.util.regex.Matcher matcher(java.lang.CharSequence)>")){
+
 	    System.out.println("SINK: " + stmt);
+
+	    /*
 	    // Get the regular expression string from the matcher.
 	    InvokeExpr invokeExpr = stmt.getInvokeExpr();
 	    InstanceInvokeExpr instanceInvokeExpr = (InstanceInvokeExpr)invokeExpr;
@@ -64,6 +84,7 @@ class TaintWrapper extends AbstractTaintWrapper {
 	    if(this.regexAnalyzer.isEvilRegex(regex)){
 		    System.out.println("VULNERABILITY: Potential REDoS vulnerability found: " + stmt);
 	    }
+	    */
 	}
 	return taints;
     }
@@ -83,8 +104,9 @@ class TaintWrapper extends AbstractTaintWrapper {
 	return true;
     }
 
-    public String getRegex(InstanceInvokeExpr instanceInvokeExpr){
-	List<Value> args = instanceInvokeExpr.getArgs();
+    public String getRegex(InvokeExpr invokeExpr){
+
+	List<Value> args = invokeExpr.getArgs();
 	String regex = "";
 	if(args.size() > 0){
 	     regex = args.get(0).toString();
